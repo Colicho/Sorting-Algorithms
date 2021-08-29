c7add238413ec3ffabbd86bc411b470f07371908
david <david.ye01@hotmail.com>
2021-08-29 18:56:50 +0200

    Graphics

--- src/sortalgos/insertionsort.js
+++ src/sortalgos/insertionsort.js
@@ -2,18 +2,26 @@
 
 function insertionSort(array, speed){
     let key = 0
-    for(let i = 1;i<array.length;i++){
-        key = array[i]
-        for(let j=i-1;j>=0;j--){
-            if (array[j] > key){
-                array[j+1] = array[j]
-            }
-            else{
-                array[j+1] = key
-                break
+    const sleep = (milliseconds) => {
+        return new Promise(resolve => setTimeout(resolve, milliseconds))
+    }
+    const doSomething = async () => {
+        for(let i = 1;i<array.length;i++){
+            key = array[i]
+            for(let j=i-1;j>=0;j--){
+                await sleep(speed)
+                document.getElementById('bars').innerHTML = array
+                if (array[j] > key){
+                    array[j+1] = array[j]
+                }
+                else{
+                    array[j+1] = key
+                    break
+                }
             }
         }
     }
+    doSomething()
     return array
 }
 
